dfb16d90d2d440d36ef84501e23ac91ebe1f8514
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-09 17:25:36 -0500

    Add the data from the API to the Redux store

--- app/src/components/MarsPhoto.js
+++ app/src/components/MarsPhoto.js
@@ -3,8 +3,11 @@ import React, {useEffect} from 'react';
 const MarsPhoto = props => {
     useEffect(() => {
         props.load();
-    }, [])
+    }, []);
+
+    const photos = props.data.photos;
     return (<div className="mars-photo">
+        <p>Number of photos: {photos.length}</p>
     </div>)
 }
 
